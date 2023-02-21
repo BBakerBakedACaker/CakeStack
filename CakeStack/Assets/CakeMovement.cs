2966873e43687bf91a15f9947823b85967c30db7
Brandon Baker <bbakerbakedacaker@gmail.com>
2023-02-20 20:41:05 -0600

    initial setup part 2
    initial setup part 2

---
+++ CakeStack/Assets/CakeMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CakeMovement : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the cake slice left and right
+        moveCake();
+        
+    }
+
+    void moveCake() {
+        // Getting the x position
+    }
+}
